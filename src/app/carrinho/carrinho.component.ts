6b8941d98d0f804129f8939f6a38ca62bbd40d84
CarlosHenriqueDv <cshenrique02@gmail.com>
2020-09-21 09:52:04 -0300

    2 files modified

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CarrinhoService } from '../carrinho.service';
 
@@ -10,14 +11,31 @@ import { CarrinhoService } from '../carrinho.service';
 export class CarrinhoComponent implements OnInit {
 
   items;
+  checkoutForm;
 
   constructor( 
-    private carrinhoService: CarrinhoService
-  ) { }
+    private carrinhoService: CarrinhoService,
+    private formBuilder: FormBuilder
+  ) { 
+
+    this.checkoutForm = this.formBuilder.group({
+      nome:'',
+      endereco:'',
+    })
+   }
 
   ngOnInit() {
 
     this.items = this.carrinhoService.getItems();
+  }
+
+  onSubmit(custormerData){
+
+    //processo de checkout dos dados aqui
+    this.items = this.carrinhoService.limparCarrinho();
+    this.checkoutForm.reset();
+    console.warn('Seu pedido foi enviado!', custormerData);
+
   }
 
 }
