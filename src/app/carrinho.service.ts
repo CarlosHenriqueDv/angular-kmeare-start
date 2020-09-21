b2ca658b6174c67d5ed2eb432f0f3a54eeb034f8
CarlosHenriqueDv <cshenrique02@gmail.com>
2020-09-21 00:12:23 -0300

    3 files modified

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -7,6 +7,23 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CarrinhoService {
 
+  items = [];
+
   constructor() { }
 
+  adicionaNoCarrinho(product){
+    this.items.push(product);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  limparCarrinho(){
+    this.items = [];
+    return this.items;
+  }
+
+
 }
+
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,8 +13,14 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
+  adicionaNoCarrinho(product){
+    this.carrinhoService.adicionaNoCarrinho(product);
+    window.alert('Você adicionou o produto ao carrinho!')
+  }
+
   constructor( 
     private route: ActivatedRoute,
+    private carrinhoService: CarrinhoService,
   ) { }
 
   ngOnInit() {
